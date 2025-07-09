773efc9fde7008ef3192484059fbb008c4809ebb
Bjorkman <mbjorkman@wpi.edu>
2025-07-09 09:43:06 -0400

    version 3

---
+++ rainbow.js
@@ -0,0 +1,24 @@
+console.log("hello world")
+let input = document.getElementById("my-input")
+let element = document.getElementById("tswcontainer")
+console.log(element)
+let angle = 0
+let shouldrainbow = true
+function onframe(){
+    angle += 1
+   let String = "hsl("+angle+"deg, 100%, 50%)"
+   if(shouldrainbow){
+    element.style.backgroundColor = String
+   }
+    requestAnimationFrame(onframe)
+}
+onfram()
+function startrainbow(){
+    shouldrainbow = true
+}
+function onbuttonclick(event){
+console.log(input.value)
+shouldrainbow = false;
+element.style.backgroundColor = input.value
+setTimeout(startrainbow, 1000)
+}
